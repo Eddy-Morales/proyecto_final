2bf539b1ac55bcbdc466d8227be49b9ce7c22aac
Eddy_Morales <edyis1720@gmail.com>
2025-01-15 18:48:49 -0500

    Initial commit

---
+++ src/administrador.java
@@ -0,0 +1,2 @@
+public class administrador {
+}
---
+++ src/login.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class login {
+    private JTextField textField_usuario;
+    private JPasswordField passwordField1;
+    private JLabel bienvenida_lbl;
+    private JLabel usuario_lbl;
+    private JLabel password_lbl;
+    private JPanel loginPanel;
+}
